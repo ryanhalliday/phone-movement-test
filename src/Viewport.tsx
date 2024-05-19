38121292e35d3bfd4fdcc4c3c35561c4e6c6fceb
Ryan Halliday <me@ryanhalliday.com>
2024-05-19 23:11:07 +1200

    tidy

---
+++ src/Viewport.tsx
@@ -0,0 +1,31 @@
+
+import * as THREE from 'three';
+import { Canvas } from '@react-three/fiber';
+import { useEffect, useState } from 'react';
+
+function Viewport() {
+	const [orientation, setOrientation] = useState({ absolute: false, alpha: 0, beta: 0, gamma: 0 });
+
+	useEffect(() => {
+		window.addEventListener("deviceorientation", (event: DeviceOrientationEvent) => {
+			const alpha = THREE.MathUtils.degToRad(event.alpha ?? 0);
+			const beta = THREE.MathUtils.degToRad(event.beta ?? 0);
+			const gamma = THREE.MathUtils.degToRad(event.gamma ?? 0);
+
+			setOrientation({ absolute: event.absolute, alpha, beta, gamma });
+		}, true);
+	}, []);
+
+	const euler = new THREE.Euler(orientation.beta, orientation.alpha, -1 * orientation.gamma, 'YXZ');
+
+	return (
+		<Canvas>
+			<mesh rotation={euler}>
+				<boxGeometry args={[1.5, 1, 3]} />
+				<meshNormalMaterial />
+			</mesh>
+		</Canvas>
+	);
+}
+
+export default Viewport;
